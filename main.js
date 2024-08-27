f58831cee409b782bb21a31cda3dd5f2acaec7ec
Maria Battista <m.battista@urbanlabspa.com>
2024-08-27 09:55:56 +0200

    operatori

---
+++ main.js
@@ -0,0 +1,34 @@
+let num1 = 3;
+let num2 = 5; 
+let num3 = 3;
+
+// operatori aritmetici
+console.log(num1 + num2);
+console.log(num2 - num1);
+
+// operatori di confronto
+console.log(num1 < num2);
+console.log(num1 != num2);
+console.log(num1 == num3);
+
+// operatori incremento / decremento
+num1++;
+console.log(num1);
+
+// operatori logici
+console.log(num1 == num3 && num1 < num2);
+
+// operatori di assegnazione:
+
+// 1) sovrascrive il valore della variabile:
+console.log(num1 = 8);
+// 2) aggiunge al valore della variabile  
+console.log(num1 += 5);
+
+
+
+
+
+
+
+
